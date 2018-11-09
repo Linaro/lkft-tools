Linaro/lkft-tools

commit b288578446c6fd0f82dc7b77ef188dd7b1518c26

Convert generate_lts_report to use Builds class
This has the advantage of not stopping at the first page of builds, so
that old builds can be accessed.

Example before this change:

    drue@xps:~/src/lkft-tools$ generate_lts_report.py 5.0 --build 8685
    Build 8685 not found

After:

    drue@xps:~/src/lkft-tools$ generate_lts_report.py 5.0 --build 8685
    Results from Linaro’s test farm.
    Regressions detected.

In Builds class, use raise_for_status() for error handling instead of
checking the return code directly.

Signed-off-by: Dan Rue <dan.rue@linaro.org>

bin/generate_lts_report.py
@@ -53,9 +53,7 @@ def detect_baseline(build_result, builds_url):
 
     # Find the previous release, or, where patch number decriments in the event
     # there was not a tagged release.
-    r = requests.get(builds_url)
-    r.raise_for_status()
-    for build in r.json()['results'][1:]:
+    for build in squad_client.Builds(builds_url):
         (build_major, build_minor, build_patch, build_patch_count, build_sha) = extract_version_info(build['version'])
         if build_patch_count is None:
             # Release version is found
@@ -69,24 +67,21 @@ def detect_baseline(build_result, builds_url):
 
 
 def get_build_report(project_url, unfinished=False,
-                     baseline=None, build=None):
+                     baseline=None, build_id=None):
     """ Given a project URL, return a test report """
 
 
     report = ""
-    builds_url = urljoin(project_url, 'builds')
-    r = requests.get(builds_url)
-    r.raise_for_status()
-    if build:
-        for build_result in r.json()['results']:
-            if int(build_result['id']) == int(build):
-                break
-        else:
-            sys.exit("Build {} not found".format(build))
-    else:
-        build_result = r.json()['results'][0]
 
-    # Check status, make sure it is finished
+    builds_url = urljoin(project_url, 'builds')
+    build_result = None
+    for build in squad_client.Builds(builds_url):
+        if int(build['id']) == int(build_id):
+            build_result = build
+            break
+    else:
+        sys.exit("Build {} not found".format(build_id))
+
     r = requests.get(build_result['status'])
     r.raise_for_status()
     status = r.json()
@@ -138,14 +133,14 @@ if __name__ == "__main__":
     force_good = args.force_good
     unfinished = args.unfinished
     baseline = args.baseline
-    build = args.build
+    build_id = args.build
     branch = args.branch
     if branch not in available_branches:
         sys.exit("Invalid branch specified")
 
     report = ""
     report = get_build_report(projects[branch], unfinished=unfinished,
-                              baseline=baseline, build=build)
+                              baseline=baseline, build_id=build_id)
 
     if branch == '4.4':
 

lib/squad_client.py
@@ -50,6 +50,7 @@ class Builds(object):
 
     def __iter__(self):
         obj_r = requests.get(self.builds_url)
+        obj_r.raise_for_status()
         objs = obj_r.json()
         while True:
             for obj in objs['results']:
@@ -58,8 +59,8 @@ class Builds(object):
                 break
             else:
                 obj_r = requests.get(objs['next'])
-                if obj_r.status_code == 200:
-                    objs = obj_r.json()
+                obj_r.raise_for_status()
+                objs = obj_r.json()
 
 class Build(object):
     def __init__(self, build_url):

commit 9e11f36e8c4ea7660128e5f3a3d19915c35100c9

generate_lts_report.py: Fix when no --build
Commit b288578446c6 ("Convert generate_lts_report to use Builds class")
broke the default behavior that returns the first available build when
--build is not set. Add a conditional for this case.

Signed-off-by: Dan Rue <dan.rue@linaro.org>

bin/generate_lts_report.py
@@ -70,12 +70,15 @@ def get_build_report(project_url, unfinished=False,
                      baseline=None, build_id=None):
     """ Given a project URL, return a test report """
 
-
     report = ""
 
     builds_url = urljoin(project_url, 'builds')
     build_result = None
     for build in squad_client.Builds(builds_url):
+        if not build_id:
+            build_result = build
+            break
+
         if int(build['id']) == int(build_id):
             build_result = build
             break