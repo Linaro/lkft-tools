bf40dec2f28fc57225e552e2e3ef0e55a0cb2a5b
Dan Rue <dan.rue@linaro.org>
2018-12-28 12:09:59 -0600

    Make tests black, too
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- tests/test_lkft_notify_developer.py
+++ tests/test_lkft_notify_developer.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-sys.path.append(os.path.join(sys.path[0], '../', 'bin'))
+sys.path.append(os.path.join(sys.path[0], "../", "bin"))
 import lkft_notify_developer
+
 
 def test_get_branch_from_make_kernelversion():
     result = lkft_notify_developer.get_branch_from_make_kernelversion("4.4.138")
@@ -16,4 +17,3 @@ def test_get_branch_from_make_kernelversion():
 
     result = lkft_notify_developer.get_branch_from_make_kernelversion("4.14.138-rc1")
     assert result == "4.14"
-
