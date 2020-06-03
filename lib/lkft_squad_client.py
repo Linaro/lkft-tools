971010a849b973f2cf6579969ff6a709a8dd9866
Daniel Díaz <daniel.diaz@linaro.org>
2020-06-03 17:09:41 -0500

    Add 5.7 kernel branch; bump mainline to 5.8
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- lib/lkft_squad_client.py
+++ lib/lkft_squad_client.py
@@ -21,10 +21,11 @@ def get_projects_by_branch():
         "5.4": "https://qa-reports.linaro.org/api/projects/232/",
         "5.5": "https://qa-reports.linaro.org/api/projects/263/",
         "5.6": "https://qa-reports.linaro.org/api/projects/284/",
+        "5.7": "https://qa-reports.linaro.org/api/projects/310/",
         # Refer to mainline by its version number
         # This is necessary so that lkft_notify_developer can determine
         # which branch to use
-        "5.7": "https://qa-reports.linaro.org/api/projects/22/",
+        "5.8": "https://qa-reports.linaro.org/api/projects/22/",
         "next": "https://qa-reports.linaro.org/api/projects/6/",
     }
 
