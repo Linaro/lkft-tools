673267ccd2f61703bfbd91541728dabc6c9fa8a3
Daniel Díaz <daniel.diaz@linaro.org>
2022-05-27 08:47:11 -0500

    Add 5.18 kernel branch; bump mainline to 5.19
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- lib/lkft_squad_client.py
+++ lib/lkft_squad_client.py
@@ -35,10 +35,11 @@ def get_projects_by_branch():
         "5.15": "https://qa-reports.linaro.org/api/projects/1022/",
         "5.16": "https://qa-reports.linaro.org/api/projects/1107/",
         "5.17": "https://qa-reports.linaro.org/api/projects/1154/",
+        "5.18": "https://qa-reports.linaro.org/api/projects/1198/",
         # Refer to mainline by its version number
         # This is necessary so that lkft_notify_developer can determine
         # which branch to use
-        "5.18": "https://qa-reports.linaro.org/api/projects/22/",
+        "5.19": "https://qa-reports.linaro.org/api/projects/22/",
         "next": "https://qa-reports.linaro.org/api/projects/6/",
     }
 
