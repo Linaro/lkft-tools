Linaro/lkft-tools

commit 3965fdce35f8a1f1cb47277a6025dad55107eefb

bin: lore-helper: use datetime instead of pytz
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

bin/lore-helper.py
@@ -4,7 +4,6 @@
 import argparse
 import datetime
 import os
-import pytz
 import sys
 import re
 
@@ -69,12 +68,12 @@ if __name__ == "__main__":
         "--since",
         help="Look as far as the given date (UTC).",
         type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d").replace(
-            tzinfo=pytz.utc
+            tzinfo=datetime.UTC
         ),
     )
     args = ap.parse_args()
 
-    NOW = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+    NOW = datetime.datetime.now(datetime.UTC)
     if not args.since:
         limit = datetime.timedelta(days=args.days)
         DT_LIMIT = NOW - limit

commit 7d1ddbc40217b2f0a6091255ed311757849c3216

bin: lore-helper: fix UTC
Should have used datetime.timezone.utc.

Fixes: 3965fdce35f8 ("bin: lore-helper: use datetime instead of pytz")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

bin/lore-helper.py
@@ -68,12 +68,12 @@ if __name__ == "__main__":
         "--since",
         help="Look as far as the given date (UTC).",
         type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d").replace(
-            tzinfo=datetime.UTC
+            tzinfo=datetime.timezone.utc
         ),
     )
     args = ap.parse_args()
 
-    NOW = datetime.datetime.now(datetime.UTC)
+    NOW = datetime.datetime.now(datetime.timezone.utc)
     if not args.since:
         limit = datetime.timedelta(days=args.days)
         DT_LIMIT = NOW - limit